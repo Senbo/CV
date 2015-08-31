fa17cc9e80a8d8dbc0fbc9df2f233030cc28773c
Michał Matysiak <matysiakmichal.fx@gmail.com>
2015-08-31 23:03:43 +0200

    fix animation

--- media/js/script.js
+++ media/js/script.js
@@ -2,24 +2,18 @@ $(document).ready(function(){
   $('.contact').animate({'margin-left': '-37px'}, 3000, 'easeOutQuart');
   $('.contact').fadeIn({queue: false, duration: 1500});
   $('.contact').css('display', 'block');
-  
-  //var delaytime = 600;
-  
-  //$('.left li').each(function() {
-  //	  $(this).delay(delaytime).animate({'margin-top': '0px'}, 3000, 'easeOutQuart');
-  //	  delaytime += 600;
-  //  });
-  
-  //$('.right li').each(function() {
-	  //$(this).delay({queue: false, delaytime}).animate({'margin-top': '0px'}, 3000, 'easeOutQuart');
-	  //delaytime += 600;
-  //});
-  
-  function delayAnimate() {
-	  var delaytime = 600;
-	  $(this).delay({queue: false, delaytime}).animate({'margin-top': '0px'}, 3000, 'easeOutQuart');
-	  delaytime += 600;
+
+  function delayAnimate(e) {
+	  $(e).each(function(index, value) {
+      var $el = $(this);
+      setTimeout(function () {
+        $el.animate({'margin-top': '0px'}, 3000, 'easeOutQuart')
+          .fadeIn({queue: false, duration: 1500})
+          .css('display', 'block');
+      }, 600 * (index + 1));
+	  });
   }
-  
-  $('.left li').each(delayAnimate);
+
+  delayAnimate('.left li');
+  delayAnimate('.right li');
 });
